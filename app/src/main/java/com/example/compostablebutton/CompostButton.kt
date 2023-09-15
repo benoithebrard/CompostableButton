74b9473999bc18df27a494792651ad90f37e2433
Benoit Hebrard <benoit.hebrard@gmail.com>
2023-09-15 16:02:09 +0200

    odds name alignment

--- app/src/main/java/com/example/compostablebutton/CompostButton.kt
+++ app/src/main/java/com/example/compostablebutton/CompostButton.kt
@@ -2,9 +2,7 @@ package com.example.compostablebutton
 
 import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedButton
@@ -12,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -49,18 +48,23 @@ fun CompostButton(
         )
     ) {
         Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
+            modifier = Modifier.fillMaxWidth()
         ) {
-            Text(
-                text = "Click Me",
-                fontSize = 10.sp
-            )
-
-            Text(
-                text = "Bitch!",
-                fontSize = 10.sp
-            )
+            Box(Modifier.weight(1f)) {
+                Text(
+                    text = "Click Me",
+                    fontSize = 10.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+            Spacer(modifier = Modifier.size(4.dp))
+            Box {
+                Text(
+                    text = "Bitch!",
+                    fontSize = 10.sp
+                )
+            }
         }
     }
 }
