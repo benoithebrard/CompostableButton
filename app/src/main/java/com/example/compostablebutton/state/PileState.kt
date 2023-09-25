d139a62802246249e4b58f7d85565585450046bb
Benoit Hebrard <benoit.hebrard@gmail.com>
2023-09-25 18:10:28 +0200

    cancelling jobs

--- app/src/main/java/com/example/compostablebutton/state/PileState.kt
+++ app/src/main/java/com/example/compostablebutton/state/PileState.kt
@@ -4,8 +4,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+
+private const val MAX_ANIMATION_TIME_MS = 5000L
 
 data class PileState(
     val id: String,
@@ -14,23 +17,33 @@ data class PileState(
     var percentFull: Int by mutableStateOf(0)
         private set
 
+    var percentFullState: PercentFullState by mutableStateOf(PercentFullState.Default)
+
+    var percentDecay: Int by mutableStateOf(0)
+
+    var lastAnimationResetJob: Job? by mutableStateOf(null)
+
     fun changePercentFullBy(scope: CoroutineScope, deltaPercent: Int) {
         val previousPercentFull = percentFull
         percentFull += deltaPercent
         percentFull = percentFull.coerceIn(0, 100)
-        percentFullState = when {
+        percentFullState = updatePercentFullState(scope, previousPercentFull)
+    }
+
+    private fun updatePercentFullState(
+        scope: CoroutineScope,
+        previousPercentFull: Int
+    ): PercentFullState {
+        return when {
             percentFull > previousPercentFull -> PercentFullState.Increasing
             percentFull < previousPercentFull -> PercentFullState.Decreasing
             else -> null
         }?.also {
-            scope.launch {
-                delay(3000)
+            lastAnimationResetJob?.cancel()
+            lastAnimationResetJob = scope.launch {
+                delay(MAX_ANIMATION_TIME_MS)
                 percentFullState = PercentFullState.Default
             }
         } ?: PercentFullState.Default
     }
-
-    var percentFullState: PercentFullState by mutableStateOf(PercentFullState.Default)
-
-    var percentDecay: Int by mutableStateOf(0)
 }
